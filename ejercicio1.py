9df546d928ac2179f06292fd12ef919fd3e9c30b
Javier <00076015@uca.edu.sv>
2018-10-20 10:15:24 -0600

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+name = input("Favor de ingresar su nombre:")
+age = int(input("Favor de ingresar su edad"))
+year = str((2018 - age)+100)
+print(name + " Usted cumplira los 100 anios en el:" + year)
